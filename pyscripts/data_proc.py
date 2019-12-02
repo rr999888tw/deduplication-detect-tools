371429659f9249341d6dff855334163100624c69
rr999888tw <rr999888tw@gmail.com>
2019-12-02 13:44:22 -0600

    add local vm

--- pyscripts/data_proc.py
+++ pyscripts/data_proc.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import statistics
+import numpy as np
 
 class Entry:
     
@@ -66,19 +67,47 @@ def read_logfile(logfile, cache, isDedup, thresh=1000):
 # 'logs/logs-without-dedup-3'
 # cache = True
 
-x4, y4 = read_logfile('vmlogs/vm04', cache=True, isDedup=True)
-x5, y5 = read_logfile('vmlogs/vm05', cache=True, isDedup=True)
-x6, y6 = read_logfile('vmlogs/vm06', cache=True, isDedup=True)
-x7, y7 = read_logfile('vmlogs/vm07', cache=True, isDedup=False)
-x8, y8 = read_logfile('vmlogs/vm08', cache=True, isDedup=False)
-x9, y9 = read_logfile('vmlogs/vm09', cache=True, isDedup=False)
+# x4, y4 = read_logfile('vmlogs/vm04', cache=True, isDedup=True)
+# x5, y5 = read_logfile('vmlogs/vm05', cache=True, isDedup=True)
+# x6, y6 = read_logfile('vmlogs/vm06', cache=True, isDedup=True)
+# x7, y7 = read_logfile('vmlogs/vm07', cache=True, isDedup=False)
+# x8, y8 = read_logfile('vmlogs/vm08', cache=True, isDedup=False)
+# x9, y9 = read_logfile('vmlogs/vm09', cache=True, isDedup=False)
 
+x1, y1 = read_logfile('local-vmlogs/dedup-1', cache=True, isDedup=True)
+x2, y2 = read_logfile('local-vmlogs/dedup-2', cache=True, isDedup=True)
+x3, y3 = read_logfile('local-vmlogs/dedup-3', cache=True, isDedup=True)
+x4, y4 = read_logfile('local-vmlogs/dedup-4', cache=True, isDedup=True)
+
+x5, y5 = read_logfile('local-vmlogs/no-dedup-1', cache=True, isDedup=False)
+x6, y6 = read_logfile('local-vmlogs/no-dedup-2', cache=True, isDedup=False)
+x7, y7 = read_logfile('local-vmlogs/no-dedup-3', cache=True, isDedup=False)
+x8, y8 = read_logfile('local-vmlogs/no-dedup-4', cache=True, isDedup=False)
+
+print (statistics.mean(y1))
+print (statistics.mean(y2))
+print (statistics.mean(y3))
 print (statistics.mean(y4))
 print (statistics.mean(y5))
 print (statistics.mean(y6))
 print (statistics.mean(y7))
 print (statistics.mean(y8))
-print (statistics.mean(y9))
+
+print (np.corrcoef(x1, y1))
+print (np.corrcoef(x2, y2))
+print (np.corrcoef(x3, y3))
+print (np.corrcoef(x4, y4))
+print (np.corrcoef(x5, y5))
+print (np.corrcoef(x6, y6))
+print (np.corrcoef(x7, y7))
+print (np.corrcoef(x8, y8))
+
+# print (np.corrcoef(x4, y4))
+# print (np.corrcoef(x5, y5))
+# print (np.corrcoef(x6, y6))
+# print (np.corrcoef(x7, y7))
+# print (np.corrcoef(x8, y8))
+# print (np.corrcoef(x9, y9))
 
 # plt.plot(x1, y1, 'bo', x2, y2, 'ro')
 # plt.show()
